6ca427b9651e6a77fafbdef0004409e38be3c9ec
mohammademon393 <mdemon357ep@gmail.com>
2025-12-08 22:56:12 +0600

    all models card design

--- src/Components/AMComponents/ModelsCard.jsx
+++ src/Components/AMComponents/ModelsCard.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
 
-const ModelsCard = () => {
-    const data = useLoaderData()
-    console.log(data);
-    
-    return (
-        <div>
-            <h2>models card</h2>
+const ModelsCard = ({ model }) => {
+    const {name, category, thumbnail, description} = model;
+  return (
+    <div className="card bg-base-100 shadow-md">
+      <figure className="p-4">
+        <img
+          className="rounded-md h-[250px] w-full"
+          src={thumbnail}
+          alt={name}
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>category: {category}</p>
+        <div className="card-actions">
+          <button className="btn btn-primary w-full">View Details</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ModelsCard;
--- src/Pages/AllModels.jsx
+++ src/Pages/AllModels.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ModelsCard from '../Components/AMComponents/ModelsCard';
+import { useLoaderData } from 'react-router';
 
 const AllModels = () => {
+    const promise =  useLoaderData();
     return (
-        <div>
-            <h2>all models</h2>
-            <ModelsCard></ModelsCard>
+      <div>
+        <h2 className="font-bold text-3xl text-center my-5">
+          <span className="text-primary">All</span> Models
+        </h2>
+        <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {promise.map((model) => (
+            <ModelsCard model={model} key={model._id}></ModelsCard>
+          ))}
         </div>
+      </div>
     );
 };
 
